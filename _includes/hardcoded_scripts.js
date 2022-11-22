d8185016c93c30d122420de224ec10d1c0f1124b
Feelcame <feelcame@gmail.com>
2022-11-22 21:20:29 +0200

    Update hardcoded_scripts.js

--- _includes/hardcoded_scripts.js
+++ _includes/hardcoded_scripts.js
@@ -1,7 +1,6 @@
 // переключатель кнопки навигации
 function showNav() {
 document.getElementById('nav').style.display='block'; 
-document.getElementById('navButton').style.display='none';
-return(false);
+return false;
 }
 
