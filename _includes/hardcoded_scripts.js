cd4c9bfb315788533fbc62e2760b265877df5d96
Feelcame <feelcame@gmail.com>
2022-11-22 23:34:25 +0200

    Update hardcoded_scripts.js

--- _includes/hardcoded_scripts.js
+++ _includes/hardcoded_scripts.js
@@ -4,3 +4,12 @@ document.getElementById('nav').style.display='block';
 return false;
 }
 
+// переключатель кнопки поиска
+function showSearch(){
+	let inputForm = document.querySelector('#search-input-form');
+	let input = document.querySelector('#search-input');
+	inputForm.style.display='block'; 
+	input.focus(); 
+	input.scrollIntoView();
+	return false;
+}
