65481a1f6b7166a0b5f3ea739c310af125032723
Feelcame <feelcame@gmail.com>
2026-05-28 18:41:20 +0300

    loop
    handle oneclick restart if crached
    not leave opened console

--- rundebug.cmd
+++ rundebug.cmd
@@ -1,9 +1,16 @@
 @echo off
 @chcp 65001 >nul
 cd /d "%~dp0"
+:loop
+cls
 echo Starting. Please wait...
+echo %CD%
 echo jekyll serve --port 80 --open-url localhost:80
-cmd /k "jekyll serve --port 80 --open-url localhost:80"
+echo ===============================================
+cmd /c "jekyll serve --port 80 --open-url localhost:80" < nul
+if NOT %ERRORLEVEL% EQU 0 (
+echo ===============================================
+echo Error code: %ERRORLEVEL%
 pause
-start "" "%~f0"
-exit /b
+)
+goto loop
